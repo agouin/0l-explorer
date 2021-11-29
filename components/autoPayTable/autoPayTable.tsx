978eba8e13ffcb190e8475037c327c2913b6aa2f
agouin <gnudrew25@gmail.com>
2021-11-29 10:10:29 -0700

    Update auto pay table per feedback

--- components/autoPayTable/autoPayTable.tsx
+++ components/autoPayTable/autoPayTable.tsx
@@ -13,16 +13,16 @@ interface AutoPayTableProps {
 
 const AutoPayNotes = {
   1: 'Iqlusion Engineering Fund, https://github.com/iqlusioninc/0L-iqlusion-engineering-fund',
-  2: 'Fulltime Engineers Program',
+  2: 'Fulltime Engineers Program https://github.com/iqlusioninc/0l-iqlusion-fulltime-engineering',
   3: 'A Good List, automated donations to non-profits, https://github.com/LOL-LLC/a-good-list',
   4: 'Tip Jar, https://github.com/LOL-LLC/tip-jar',
-  5: 'Benefits, Blockscience',
+  5: 'Benefits, Blockscience https://github.com/BlockScience/deep-technology-innovation-program https://github.com/BlockScience/social-infrastructure-program',
   6: 'Humanitarian, Red Cross, https://github.com/Danish-Red-Cross-Humanitarian-Fund',
   7: 'App Studio, Newlab, https://github.com/blockchainnewlab/Application-Studio/',
   8: 'Moonshots, LOL, https://github.com/LOL-LLC/moonshot-program',
   9: 'Human Rewards',
   10: 'RadicalxChange Foundation, https://github.com/RadicalxChange/RxC-Research-and-Experimentation',
-  11: 'UofT MSRG',
+  11: 'UofT MSRG https://github.com/MSRG/D',
 }
 
 const getNoteWithLink = (note) => (
@@ -57,7 +57,7 @@ const AutoPayTable = ({
       key: 'note',
       title: 'Note',
       dataIndex: 'note',
-      width: 300,
+      width: 500,
       render: (_, record: AutoPayPayments) =>
         getNoteWithLink(AutoPayNotes[record.uid]),
     },
@@ -67,27 +67,6 @@ const AutoPayTable = ({
       width: 300,
       title: 'Payee',
       render: (text) => <a href={`/address/${text}`}>{text}</a>,
-    },
-    {
-      key: 'type_desc',
-      title: 'Type',
-      dataIndex: 'type_desc',
-      sorter: Sorter((record) => record.type_desc),
-      width: 150,
-    },
-    {
-      key: 'end_epoch',
-      title: 'End Epoch',
-      dataIndex: 'end_epoch',
-      sorter: Sorter((record) => record.end_epoch),
-      width: 120,
-    },
-    {
-      key: 'amount',
-      title: 'Amount',
-      dataIndex: 'amount',
-      sorter: Sorter((record) => parseInt(record.amount)),
-      width: 120,
     },
     {
       key: 'validators',
